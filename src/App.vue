35cc46029d315d32e9e75a1eab2fd6b8415ae746
17LingShan <huangruofeng9@gmail.com>
2023-03-12 22:22:35 +0800

    :tada: first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script lang="ts" setup>
+import { LButton } from "./components";
+</script>
+
+<template>
+  <LButton size="small">xihuan</LButton>
+  <LButton size="large">xihuan</LButton>
+</template>
+
+<style lang="scss" scoped></style>
